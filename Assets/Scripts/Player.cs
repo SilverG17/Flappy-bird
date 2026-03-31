35883078b91e1fa0104126e945fd949b8b67c1b9
SilverG17 <silvergaster@gmail.com>
2026-03-31 17:54:06 +0700

    Add hitbox

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 direction;
+    public float gravity = -9.81f;
+
+    void Update()
+    {
+        // Called once per frame
+    }
+}
